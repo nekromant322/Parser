f895d796b8403ace079fb58f4e59fd8122ca4540
Anatolii Marandiuc <shamaneeel@mail.ru>
2019-01-23 14:48:47 +0300

    Добавлен gui , не работает выбор файла

--- src/Controller.java
+++ src/Controller.java
@@ -1,6 +1,15 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
 
 public class Controller {
 
@@ -11,12 +20,57 @@ public class Controller {
     private Button chfile;
 
     @FXML
-    void StartActoin(ActionEvent event) {
-
+    void StartActoin(ActionEvent event)
+    {
+        System.out.println("start pushed");
     }
 
     @FXML
-    void ChooseAction(ActionEvent event) {
+    void ChooseAction(ActionEvent event)
+    {
+        /*Stage stage = new Stage();
+        System.out.println("action pushed");
+       // MyFrame fr = new MyFrame();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.showOpenDialog(stage);
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
+                new FileChooser.ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"),
+                new FileChooser.ExtensionFilter("All Files", "*.*");
+
+        File selectedFile = fileChooser.showOpenDialog(stage);
+
+        if (selectedFile != null)
+        {
+           System.out.println(selectedFile.toString());
+        }*/
+
+        JFileChooser chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new java.io.File("."));
+        chooser.setDialogTitle("choosertitle");
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        chooser.setAcceptAllFileFilterUsed(false);
+        FileFilter filter = new FileNameExtensionFilter("Excel file", "xls", "xlsx");
+        chooser.addChoosableFileFilter(filter);
+        if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+        {
+            System.out.println("getCurrentDirectory(): " + chooser.getCurrentDirectory());
+            System.out.println("getSelectedFile() : " + chooser.getSelectedFile());
+        }
+        else
+        {
+            System.out.println("No Selection ");
+        }
+
+
+
+
+    }
+
+    public static void main(String[] args)
+    {
 
     }
 
---
+++ src/Main.java
@@ -0,0 +1,31 @@
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
+        primaryStage.setTitle("Parser");
+        //primaryStage.getIcons().add(new Image("sample/assets/delivryicon.png"));
+
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        Controller cl = new Controller();
+        // cl.initialize();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
