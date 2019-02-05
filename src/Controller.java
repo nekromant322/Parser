998432bc1ee6bf684a56dc14249a0fdae17248f3
Anatolii Marandiuc <shamaneeel@mail.ru>
2019-02-05 23:03:38 +0300

    Скрол логов, парсинг в отдельном потоке

--- src/Controller.java
+++ src/Controller.java
@@ -3,6 +3,7 @@ import javafx.concurrent.Task;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
@@ -29,6 +30,29 @@ public class Controller
         private TextArea Log_Text;
 
         @FXML
+        private ScrollPane Scroll;
+
+        @FXML
+        void initialize()
+        {
+            Task task_log = new Task<Void>()
+            {
+                @Override
+                public Void call() throws InterruptedException
+                {
+                    while(true)
+                    {
+                        Log_Text.setText(Log);
+                        Thread.sleep(500);
+                    }
+                }
+            };
+            new Thread(task_log).start();
+
+
+        }
+
+        @FXML
         void FindAction(ActionEvent event) throws IOException
         {
 
@@ -49,25 +73,21 @@ public class Controller
             InputExcel input = new  InputExcel(inputpath);
             PrimaryParsing pp = new PrimaryParsing(input.PrimaryRef,outputpath);
 
-            pp.SearchSecRef();
-            pp.ShowLists();
 
-            Runnable task = () ->
+
+            Task task = new Task<Void>()
             {
-
-                try
+                @Override
+                public Void call() throws IOException
                 {
+                    pp.SearchSecRef();
+                    pp.ShowLists();
                     pp.FinalParsing(input.KeyWords);
+                    return null;
                 }
-                catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
-
             };
-            task.run();
-            Thread thread = new Thread(task);
-            thread.start();
+
+            new Thread(task).start();
 
             Log = Log + "Парсинг запущен в отдельном потоке" +"\n";
             Log_Text.setText(Log);
